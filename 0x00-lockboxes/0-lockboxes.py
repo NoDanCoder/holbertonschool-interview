af368669cea9d20ba7e1f55b56967179fe007490
Daniel Cumaco <dancoder@protonmail.com>
2021-02-08 23:54:57 -0500

    [Add] 0x00#0 comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+""" lockboxes module """
+
 def openBox(boxes, box, memo={0}):
+    """ itertate recursively truough list of boxes
+        and save them on memo set
+    """
     for i in boxes[box]:
         if i not in memo:
             memo.add(i)
@@ -10,6 +15,9 @@ def openBox(boxes, box, memo={0}):
 
 
 def canUnlockAll(boxes):
+    """ check box is valid then start recursive function to verify
+        if all list of boxes are opened
+    """
     if type(boxes) is list and boxes != []:
         openedBoxes = openBox(boxes, 0, {0})
         return len(boxes) == len(openedBoxes)
