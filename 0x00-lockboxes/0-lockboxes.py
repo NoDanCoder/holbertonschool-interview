ff0da2b07c0f205f138400b88aba7f9e8eeaf2f7
Daniel Cumaco <dancoder@protonmail.com>
2021-02-09 12:36:32 -0500

    [fix] 0x00#0 key opening inexistent box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,14 +2,19 @@
 
 """ lockboxes module """
 
-def openBox(boxes, box, memo={0}):
+def openBox(boxes, box, memo):
     """ itertate recursively truough list of boxes
         and save them on memo set
     """
-    for i in boxes[box]:
-        if i not in memo:
-            memo.add(i)
-            openBox(boxes, i, memo)
+    try:
+        keys = boxes[box]
+        memo.add(box)
+    except IndexError:
+        return
+
+    for key in keys:
+        if key not in memo:
+            openBox(boxes, key, memo)
 
     return memo
 
@@ -19,6 +24,6 @@ def canUnlockAll(boxes):
         if all list of boxes are opened
     """
     if type(boxes) is list and boxes != []:
-        openedBoxes = openBox(boxes, 0, {0})
+        openedBoxes = openBox(boxes, 0, set())
         return len(boxes) == len(openedBoxes)
     return False
